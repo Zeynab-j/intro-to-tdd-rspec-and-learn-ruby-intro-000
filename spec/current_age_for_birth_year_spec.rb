40936dfeea5aae56b3339f4d3fa585beb0899f6e
Zeynab Jibreel <zeina2025@gmail.com>
2020-02-20 21:53:58 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -5,6 +5,6 @@ describe "current_age_for_birth_year method" do
     age_of_person = current_age_for_birth_year(returns the
 age of a person based on the year of birth)
 
-    expect(age_of_person).to eq(current_age_for_birth_year)
+  expect(age_of_person).to eq()
   end
 end
